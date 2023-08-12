8adfb6911526923dbc6b06b3b8561e23aa4df427
Kevin Knights <kevinknights29@hotmail.com>
2023-08-12 10:17:34 -0500

    WIP: adding file upload basic layout

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
 from __future__ import annotations
 
+import os
+
+from flask import flash
 from flask import Flask
+from flask import redirect
 from flask import render_template
+from flask import request
+from flask import url_for
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "uploads")
+ALLOWED_EXTENSIONS = {"zip"}
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
-@app.route("/")
+def allowed_file(filename: str) -> bool:
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            return url_for("analyze", uploaded_filename=filename)
     return render_template("index.html")
+
+
+@app.route("/analyze/<upload_filename>")
+def analyze(upload_filename: str):
+    return render_template("analyze.html", uploaded_filename=upload_filename)

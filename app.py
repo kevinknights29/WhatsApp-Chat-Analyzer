f8b81f4d6b403f840342e1fcf8c3ea1fa916d3da
Kevin Knights <kevinknights29@hotmail.com>
2023-08-12 10:41:40 -0500

    added basic functionality

--- app.py
+++ app.py
@@ -12,9 +12,10 @@ from werkzeug.utils import secure_filename
 
 
 UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "uploads")
-ALLOWED_EXTENSIONS = {"zip"}
+ALLOWED_EXTENSIONS = {"zip", "txt"}
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16).hex()
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
@@ -25,17 +26,19 @@ def allowed_file(filename: str) -> bool:
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        if "file" not in request.files:
+        if "chatFile" not in request.files:
             flash("No file part")
             return redirect(request.url)
-        file = request.files["file"]
+        file = request.files["chatFile"]
         if file.filename == "":
             flash("No selected file")
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-            return url_for("analyze", uploaded_filename=filename)
+            return redirect(
+                url_for("analyze", upload_filename=filename, _external=True),
+            )
     return render_template("index.html")
 
 

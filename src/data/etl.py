88e9dc37c8d29467d4c46b2ada41e55fd04043b6
Kevin Knights <kevinknights29@hotmail.com>
2023-08-13 12:17:27 -0500

    added s3 extraction to pipeline

--- src/data/etl.py
+++ src/data/etl.py
@@ -6,12 +6,26 @@ import duckdb
 import pandas as pd
 import polars as pl
 
+from src.data import s3
+
 
 def extract(txt_file_path: str) -> pl.DataFrame:
     pattern = r"\[(.*?)\] (.*?): (.*)"
     with open(txt_file_path) as file:
         text = file.read()
     matches = re.findall(pattern, text)
+    columns = ["Date", "Sender", "Message"]
+    df = pd.DataFrame(matches, columns=columns)
+    return pl.DataFrame(df)
+
+
+def extract_from_s3(key: str, bucket=s3.AWS_S3_BUCKET) -> pl.DataFrame:
+    data = s3.get_data_from_s3(key, bucket_name=bucket)
+    if not data:
+        print(f"Failed to fetch data for key: {key} from bucket: {bucket}")
+        return pl.DataFrame()
+    pattern = r"\[(.*?)\] (.*?): (.*)"
+    matches = re.findall(pattern, data)
     columns = ["Date", "Sender", "Message"]
     df = pd.DataFrame(matches, columns=columns)
     return pl.DataFrame(df)
@@ -34,9 +48,9 @@ def load(output_file_path: str) -> duckdb.DuckDBPyConnection:
     return db
 
 
-def etl_pipeline(txt_file_path: str) -> duckdb.DuckDBPyConnection:
-    df = extract(txt_file_path)
-    output_file_path = txt_file_path.replace(".txt", ".parquet")
+def etl_pipeline(key: str, bucket=s3.AWS_S3_BUCKET) -> duckdb.DuckDBPyConnection:
+    df = extract_from_s3(key, bucket)
+    output_file_path = key.replace(".txt", ".parquet")
     output_file_path = transform(df, output_file_path)
     db = load(output_file_path)
     return db

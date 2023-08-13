5a9593c3e0fb902305e5c2583e57400ea8733cd6
Kevin Knights <kevinknights29@hotmail.com>
2023-08-13 12:16:54 -0500

    added s3 client as singleton

--- src/data/s3.py
+++ src/data/s3.py
@@ -12,16 +12,24 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_BUCKET = os.environ.get("AWS_S3_BUCKET")
 AWS_S3_LOCATION = os.environ.get("AWS_S3_LOCATION")
 
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-)
+
+__s3_client = None
+
+
+def s3_client() -> boto3.client:
+    global __s3_client
+    if __s3_client is None:
+        __s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        )
+    return __s3_client
 
 
 def upload_to_s3(file, filename, bucket_name=AWS_S3_BUCKET) -> str:
     try:
-        s3.put_object(
+        s3_client().put_object(
             Bucket=bucket_name,
             Key=filename,
             Body=file,
@@ -32,3 +40,13 @@ def upload_to_s3(file, filename, bucket_name=AWS_S3_BUCKET) -> str:
         print("Something Happened: ", e)
         return e
     return filename
+
+
+def get_data_from_s3(key, bucket_name=AWS_S3_BUCKET) -> str:
+    try:
+        obj = s3_client().get_object(Bucket=bucket_name, Key=key)
+        text = obj["Body"].read().decode("utf-8")
+        return text
+    except Exception as e:
+        print("Something Happened: ", e)
+        raise e
